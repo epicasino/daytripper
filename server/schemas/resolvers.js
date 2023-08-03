a5cb134b701077458c3871dc46cd0edddb2343ff
Ray Badua <rayjonathanbadua@gmail.com>
2023-08-03 14:11:54 -0700

    added user typedefs and resolvers in back-end

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,40 @@
-const { Tech, Matchup } = require('../models');
+const { User } = require('../models/');
+const { AuthenticationError, signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    users: async () => {
+      return await User.find({});
+    },
   },
   Mutation: {
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPass = await user.isCorrectPassword(password);
+
+      if (!correctPass) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { user, token };
+    },
+    removeUser: async (parent, { userId }) => {
+      const user = await User.findByIdAndDelete(userId);
+      if (user) {
+        return user;
+      }
+    },
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,23 @@
 const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    users: [User]
+    me: User
+  }
+  type Mutation {
+    login(username: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    removeUser(userId: String!): User
+  }
 `;
 
 module.exports = typeDefs;
