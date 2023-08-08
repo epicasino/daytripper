b4a8dd7ec74b5e47568b9800fb908d2ca39251a6
Ray Badua <rayjonathanbadua@gmail.com>
2023-08-08 16:51:16 -0700

    added styling to trips.jsx

--- client/src/pages/Trips.jsx
+++ client/src/pages/Trips.jsx
@@ -19,25 +19,46 @@ export default function Trips() {
   // Checks to see if user is logged in , if not, returns with this
   if (!Auth.loggedIn()) {
     return (
-      <div className='bg-sage h-screen font-kawaii text-white text-center align-middle object-contain'>
+      <div className="bg-sage h-screen font-kawaii text-white text-center align-middle object-contain">
         <Navbar />
-        <h1 className='m-2 p-xl'>Well, this is awkward...</h1>
+        <h1 className="m-2 p-xl">Well, this is awkward...</h1>
         <h1>You need to be logged in to see your saved trips!</h1>
       </div>
     );
   }
 
+  const styles = {
+    tripsContainer: {
+      height: '100vh',
+      width: '100vw'
+    }
+  }
+
   return (
-    <div className='bg-gradient-to-r from-terracotta via-sand to-sage bg-auto'>
+    <div className="bg-gradient-to-r from-terracotta via-sand to-sage" style={styles.tripsContainer}>
       <Navbar />
-      <header className="text-center font-kawaii text-white text-3xl p-1 bg-terracotta border-b-2 border-green ">My Saved Trips</header>
-      <h1 className='font-kawaii text-white m-4'>Hello, {userData.username}!</h1>
-        <p className='font-kawaii text-white m-4'>A look at your saved trips:</p>
-      <div className="tripsContainer border-box rounded-lg h-fit w-fit p-4 border-4 bg-green font-kawaii text-white border-sage m-2 flex flex-col items-center justify-center ">
+      <header className="text-center font-kawaii text-white text-3xl p-1 bg-terracotta border-b-2 border-green ">
+        My Saved Trips
+      </header>
+      <subheader className="text-center">
+        <h1 className="font-kawaii text-white m-4">
+          Hello, {userData.username}!
+        </h1>
+        <p className="font-kawaii text-white m-4">
+          A look at your saved trips:
+        </p>
+      </subheader>
+      <div
+        className="tripsContainer border-box rounded-lg h-fit p-4 border-4
+       bg-green font-kawaii text-white border-sage m-2 flex flex-col items-center justify-center"
+      >
         <h2>Trips</h2>
         {userData.trips?.map((trip) => {
           return (
-            <div key={trip._id} className='border-box rounded-lg h-fit w-fit p-4 border-4 bg-dirt font-kawaii text-white border-sand m-2'>
+            <div
+              key={trip._id}
+              className="border-box rounded-lg h-fit w-3/4 p-4 border-4 bg-dirt font-kawaii text-white border-sand m-2"
+            >
               <h5>From: {trip.startLocation}</h5>
               <h5>To: {trip.destinationLocation}</h5>
               <h5>Waypoints: </h5>
