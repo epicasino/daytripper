9e886183ec9a11f89e95b0447209ba11634dbf3b
Ray Badua <rayjonathanbadua@gmail.com>
2023-08-08 10:27:23 -0700

    updated styling, <Link> component in sign-in to register page

--- client/src/components/login/Registration.jsx
+++ client/src/components/login/Registration.jsx
@@ -1,25 +1,24 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 export default function RegistrationForm() {
-
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const handleInputChange = (e) => {
     const { id, value } = e.target;
 
-    if (id === "userName") {
+    if (id === 'regUserName') {
       setUserName(value);
     }
-    if (id === "email") {
+    if (id === 'regEmail') {
       setEmail(value);
     }
-    if (id === "password") {
+    if (id === 'regPassword') {
       setPassword(value);
     }
-    if (id === "confirmPassword") {
+    if (id === 'confirmPassword') {
       setConfirmPassword(value);
     }
   };
@@ -32,32 +31,66 @@ export default function RegistrationForm() {
     <form>
       <div className="form-body">
         <div className="username">
-          <label className="form__label" htmlFor="userName">Username</label>
-          <input className="form__input" type="text" value={userName}
-            onChange={(e) => handleInputChange(e)} id="userName" placeholder="userName"/>
+          <label className="form__label" htmlFor="userName">
+            Username
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            value={userName}
+            onChange={(e) => handleInputChange(e)}
+            id="regUserName"
+            placeholder="userName"
+          />
         </div>
 
         <div className="email">
-          <label className="form__label" htmlFor="email">Email</label>
-          <input type="email" id="email" className="form__input" value={email}
-            onChange={(e) => handleInputChange(e)} placeholder="Email"/>
+          <label className="form__label" htmlFor="email">
+            Email
+          </label>
+          <input
+            type="email"
+            id="regEmail"
+            className="form__input"
+            value={email}
+            onChange={(e) => handleInputChange(e)}
+            placeholder="Email"
+          />
         </div>
 
         <div className="password">
-          <label className="form__label" htmlFor="password">Password</label>
-          <input className="form__input" type="password" id="password" value={password}
-            onChange={(e) => handleInputChange(e)} placeholder="Password"/>
+          <label className="form__label" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="form__input"
+            type="password"
+            id="regPassword"
+            value={password}
+            onChange={(e) => handleInputChange(e)}
+            placeholder="Password"
+          />
         </div>
 
         <div className="confirm-password">
-          <label className="form__label" htmlFor="confirmPassword">Confirm Password</label>
-          <input className="form__input" type="password" id="confirmPassword" value={confirmPassword}
-            onChange={(e) => handleInputChange(e)} placeholder="Confirm Password"/>
+          <label className="form__label" htmlFor="confirmPassword">
+            Confirm Password
+          </label>
+          <input
+            className="form__input"
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => handleInputChange(e)}
+            placeholder="Confirm Password"
+          />
         </div>
       </div>
 
       <div className="footer">
-        <button onClick={() => handleSubmit()} type="submit" className="btn">Register</button>
+        <button onClick={() => handleSubmit()} type="submit" className="btn">
+          Register
+        </button>
       </div>
     </form>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import App from './App.jsx';
 import Home from './pages/Home.jsx';
 import Map from './pages/Map.jsx';
 import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
 
 const router = createBrowserRouter([
   {
@@ -28,6 +29,10 @@ const router = createBrowserRouter([
       {
         path: '/login',
         element: <Login />
+      },
+      {
+        path: '/register',
+        element: <Register />
       }
     ],
   },
