32adc2d95e3a1d4f8f3fc0cf8baf0a6a40710405
Ray Badua <rayjonathanbadua@gmail.com>
2023-08-09 10:28:23 -0700

    modified Map.jsx request url

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -149,7 +149,7 @@ export default function Map() {
       setSelectedLocation(location);
 
       const detailSearch = await fetch(
-        `https://daytripper-app-2d48e649b430.herokuapp.com/api/placeDetails/${e.placeId}`,
+        `https://daytripper-app-1577e78f3877.herokuapp.com/api/placeDetails/${e.placeId}`
       )
         .then((data) => data.json())
         .then((json) => json.result);
