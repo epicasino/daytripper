67704c92962ebb5dcff678973fbab2026fde09a6
Ray Badua <rayjonathanbadua@gmail.com>
2023-08-08 23:18:19 -0700

    cors error debugging

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -2,7 +2,14 @@ require('dotenv').config();
 
 const router = require('express').Router();
 
-router.get('/placeDetails/:placeId', async (req, res) => {
+router.get('/placeDetails/:placeId', async (req, res, next) => {
+
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://daytripper-app-1577e78f3877.herokuapp.com/'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET');
+  next();
 
   const response = await fetch(
     `https://maps.googleapis.com/maps/api/place/details/json?place_id=${req.params.placeId}&fields=formatted_address,name,website,rating,geometry,url&key=${process.env.GOOGLE_MAPS_API_KEY}`
